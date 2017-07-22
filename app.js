d98e16cfc378edb55a41ff44060f07853e9e4912
Alex Lee <doomcat9167@gmail.com>
2017-07-21 19:39:11 -0700

    rest api now pulls the right params for 'to' and 'from'

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ var express = require('express');
 
 var http = require('http');
 var path = require('path');
+var bodyParser = require('body-parser');
+
 var sms = require('./public/js/sms.js');
 
 var app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -14,8 +18,10 @@ app.get('/', function(req, res) {
     console.log('index');
 });
 
-app.get('/sendSMS', function(req, res) {
-    sms.sendSMS();
+app.post('/sendSMS', function(req, res) {
+    var to = req.param('To');
+    var body = req.param('Body');
+    sms.sendSMS(to, body);
 });
 
 
--- public/js/sms.js
+++ public/js/sms.js
@@ -3,11 +3,11 @@ var client = require('twilio')(
     'c4e7a3fb6a522a073e18423a04ac181c'
 );
 
-exports.sendSMS = function() {
+exports.sendSMS = function(to, body) {
     client.messages.create({
         from: '19713402317',
-        to: '15039983176',
-        body: "You just sent an SMS from Node.js using Twilio!"
+        to: to,
+        body: body
     }, function(err, message) {
         if(err) {
             console.error(err.message);
